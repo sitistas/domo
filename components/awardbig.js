f3539c2e2f4640560a1a5f5b8a10e587f1ddaac1
MaritinaBiz <maritina.biz@gmail.com>
2023-01-17 00:51:28 +0200

    new things

--- components/awardbig.js
+++ components/awardbig.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { StyleSheet, Text,Image, View, TouchableOpacity, onPress} from 'react-native';
 
 class Awardbig extends Component {
-    state = {awardSwitch: false};
+  state = {awardSwitch: false};
     render() {
     
     return(
     <View style = {styles.widgetspacing}>
         <View style={styles.awardsContainer}>
             <View style={styles.awardSubContainer}>
-                <Text style={styles.cuboidbigText2}>{'5,08 kWh ~ 0.1€'}</Text>
                 <TouchableOpacity onPress={() => {this.setState({awardSwitch: !this.state.awardSwitch})}}>
-                    <View>
-                        <Image source = {this.state.awardSwitch ? require('../componentsAssets/questionmark.svg') : require('../componentsAssets/Sea-turtle.svg')} style={styles.WidgetSwitch}/>
-                    </View>
+                  <View>
+                      <Image source = {this.state.awardSwitch ? require('./componentsAssets/turtle.png') : require('./componentsAssets/qm.png')} style={styles.bigWidgetSwitch}/>
+                  </View>
                 </TouchableOpacity>
+                <Text style = {styles.Text1}>{'Perfect Week'}</Text>
             </View>
         </View>
     </View>
@@ -28,29 +28,21 @@ export default Awardbig;
 const styles = StyleSheet.create({
     awardsContainer: {
         flexDirection: 'row',
-        borderColor:"#EAEAB9",
-        borderWidth: 5,
         width: 338,
         height: 500, 
         alignSelf: 'center'
       },
       awardSubContainer: {
         flex: 1,
-        alignSelf: "center",
         flexDirection: 'collumn',
-        paddingLeft: 15
+        paddingLeft: 5
       },
-      cuboidbigTitle:{
-        fontSize: 30,
-        color: "#000",
-        fontFamily: 'LatoBold'
-      },
-      cuboidbigText1: {
+      Text1: {
         fontSize: 15,
         color: "#000",
         fontFamily: 'LatoRegular'
       },
-      cuboidbigText2: {
+      Text2: {
         paddingBottom: 10,
         fontSize: 15,
         color: "#9AA049",
@@ -60,13 +52,9 @@ const styles = StyleSheet.create({
         paddingTop: 22,
         paddingBottom: 11
       },
-      bigWidgetImage: {
-        width: 95,
-        height: 160
+      bigWidgetSwitch: {
+        width: 85,
+        height: 86,
+        paddingBottom: 20
       },
-      WidgetSwitch: {
-        alignSelf: "left",
-        width: 35,
-        height: 36
-      }
   });
