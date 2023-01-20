0a693a561a1535cc897fc3db824ea186e66e0bad
Konstantinos Kotorenis <kostas123koto@hotmail.gr>
2023-01-21 00:15:52 +0200

    websocket: rec previous state-button d/sn't change

--- components/smallButton.js
+++ components/smallButton.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Image, TouchableOpacity, onPress} from 'react-native';
+import React, { Component, useEffect } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, onPress, RefreshControl} from 'react-native';
 
 
 
 class Smallbutton extends Component {
-  state = {outletSwitch: false};
+  state = {outletSwitch: 'wait'};
+  
+
 
   render() {
     
+
+
     var ws = new WebSocket('ws://192.168.2.7:2014');
     ws.onopen = () => {
-      
+      console.log('opened');
       var temp;
       
       ws.send('ReportStates')
@@ -23,11 +27,17 @@ class Smallbutton extends Component {
 
           if (temp.xMAC == this.props.MACadd){
             this.state.outletSwitch = temp.IsOn;
+            //this.state.isLoading = false;
+            //var result = temp.IsOn;
             console.log(temp.xMAC, this.state.outletSwitch );
+            
           }
         }
       }; 
-      
+      if (this.state.outletSwitch == 'wait'){
+        
+      }
+      else{
         if (this.state.outletSwitch)
         {
           ws.send('TurnOn {"xMAC":"' + this.props.MACadd + '","Number":10}');
@@ -36,12 +46,18 @@ class Smallbutton extends Component {
         {
           ws.send('TurnOff {"xMAC":"' + this.props.MACadd + '","Number":10}');
         }
+      
+      }
         
     } 
 
+    ws.onclose = e => {
+      // connection closed
+      //console.log(e.code, e.reason);
+    };
     
+    //setTimeout(() => {  console.log("Wait"); }, 5000);
 
-    
     return(
       <View style = {styles.widgetSpacing}>
         <TouchableOpacity onPress={() => {
@@ -56,11 +72,7 @@ class Smallbutton extends Component {
         </TouchableOpacity>
       </View>
     );
-    
-      
-
-    
-  }
+  }  
 }
 
 export default Smallbutton;
